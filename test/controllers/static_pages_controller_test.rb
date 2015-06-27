2dbf4ccfa368e4ca33352ea28359f54b9cb5d0a3
cthad <lohthaddeus@gmail.com>
2015-06-28 03:17:32 +0800

    Finished layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Natif"
+    assert_select "title", "Natif"
   end
 
   test "should get help" do
@@ -18,5 +18,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | Natif"
   end
+
+  test "should get contact" do
+  	get :contact
+  	assert_response :success
+  	assert_select "title", "Contact | Natif"
+  end 
 
 end
